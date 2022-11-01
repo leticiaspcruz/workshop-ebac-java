19c665e85ce1216b32544a27bfc9d3694e53a621
leticiaspcruz <leticiaspcruz@gmail.com>
2022-10-31 21:44:15 -0300

    task: creating a project with spring boot

---
+++ src/test/java/com/example/workshopjavaebac/WorkshopJavaEbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopjavaebac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJavaEbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
